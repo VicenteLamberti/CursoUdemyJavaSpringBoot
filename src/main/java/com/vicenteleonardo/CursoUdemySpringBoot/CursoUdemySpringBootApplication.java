3b60d8506551b9c62ffc3c3468547ca7bc89bd23
VicenteLamberti <vicente.lamberti@hotmail.com>
2021-12-21 20:33:28 -0300

    Criação Projeto

---
+++ src/main/java/com/vicenteleonardo/CursoUdemySpringBoot/CursoUdemySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.vicenteleonardo.CursoUdemySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUdemySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUdemySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vicenteleonardo/CursoUdemySpringBoot/CursoUdemySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicenteleonardo.CursoUdemySpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoUdemySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
