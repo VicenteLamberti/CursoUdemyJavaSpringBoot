6ea0e3151ecedf3cd345760506fa0a05c5118d31
VicenteLamberti <vicente.lamberti@hotmail.com>
2022-01-30 21:11:28 -0300

    Criando operação de instanciação

--- src/main/java/com/vicenteleonardo/CursoUdemySpringBoot/CursoUdemySpringBootApplication.java
+++ src/main/java/com/vicenteleonardo/CursoUdemySpringBoot/CursoUdemySpringBootApplication.java
@@ -1,13 +1,32 @@
 package com.vicenteleonardo.CursoUdemySpringBoot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoUdemySpringBootApplication {
+import com.vicenteleonardo.CursoUdemySpringBoot.domain.Categoria;
+import com.vicenteleonardo.CursoUdemySpringBoot.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoUdemySpringBootApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repo; 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoUdemySpringBootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		repo.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
