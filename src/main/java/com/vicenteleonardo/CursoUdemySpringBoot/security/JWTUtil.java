60eba088a540a80f6c45851430d677f305417d4e
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2022-12-14 20:54:53 -0300

    Adicionando autenticacao

---
+++ src/main/java/com/vicenteleonardo/CursoUdemySpringBoot/security/JWTUtil.java
@@ -0,0 +1,28 @@
+package com.vicenteleonardo.CursoUdemySpringBoot.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JWTUtil {
+
+	@Value("${jwt.secret}")
+	private String secret;
+	
+
+	@Value("${jwt.expiration}")
+	private String expiration;
+	
+	public String generateToken(String username) {
+		return Jwts.builder()
+				.setSubject(username)
+				.setExpiration(new Date(new Date().getTime() + Long.parseLong(expiration)))
+				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
+				.compact();
+	}
+}
