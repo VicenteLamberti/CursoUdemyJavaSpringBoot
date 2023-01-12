b87d6f7a0a7edcdda65a3f9d3f08c1edcbdc7e27
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-01-12 16:19:37 -0300

    Refresh Token

---
+++ src/main/java/com/vicenteleonardo/CursoUdemySpringBoot/resources/AuthResource.java
@@ -0,0 +1,30 @@
+package com.vicenteleonardo.CursoUdemySpringBoot.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vicenteleonardo.CursoUdemySpringBoot.security.JWTUtil;
+import com.vicenteleonardo.CursoUdemySpringBoot.security.UserSS;
+import com.vicenteleonardo.CursoUdemySpringBoot.services.UserService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@PostMapping("/refresh_token")
+	public ResponseEntity<Void>refreshToken(HttpServletResponse response){
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
